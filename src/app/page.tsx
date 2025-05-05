67ecafd674ad0345163404e9a710c384d4cd35ce
Liam Snyder <MackMaritimus@outlook.com>
2025-05-05 06:20:58 -0600

    Day 3 - More Styling and First Partially Responsive Design
    - Add Profile Picture
    - Added Gradient to Buttons Section
    - Added Gradients to Buttons
    - Header Resizes Responsively
    - Profile Picture Hides Responsively
    - Buttons Resize Responsively
    
    Responsiveness Works (partially) With:
    
    - IPhone 14 Pro Max
    - Macbook Pro 14" (fullscreen)
    - Desktop (fullscreen)
    - A Number of Other Untested Devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,53 +4,67 @@ export default function HomePage() {
     <main className = "min-h-screen bg-zinc-900 text-white flex flex-col items-center space-y-16">
       
       {/* Summary Section */}
-      <section className = "text-center max-w-4xl mt-16">
-        <div className = "w-4xl mx-auto h-[150px] bg-zinc-800 flex items-center justify-center rounded-lg">
-          <h1 className = "text-4xl font-bold">Liam Mack Snyder</h1>
+      <section className = "relative flex max-w-full mt-16">
+        {/* Summary Section Text */}
+        <div className = "flex flex-col text-center justify-center max-w-full">
+          {/* Page Header Background and Text */}
+          <div className = "max-w-4xl px-10 md:px-48 2xl:px-58 mx-auto h-[150px] bg-zinc-700 flex items-center justify-center rounded-lg">
+            <h1 className = "text-3xl md:text-4xl font-bold">Liam Mack Snyder</h1>
+          </div>
+          <p className = "max-w-2xl px-4 text-lg mx-auto text-gray-300 mt-4 mb-6">
+            Full-stack developer and designer with a dual focus in Computer Information Systems and Marketing. I build fast, clean web experiences from the ground up.
+          </p>
+        </div> 
+        {/* Summary Section Image Border and Image */}
+        <div className = "hidden xl:block absolute right-[-47.5%] 2xl:right-[-55%] px-4 py-4 bg-radial from-slate-800 to-zinc-800 rounded-xl shadow-xl">
+          <img 
+            src = "/Backhat 2.png" 
+            alt = "Profile Picture" 
+            className = "w-64 h-64 object-contain mx-auto rounded-xl shadow-xl"
+          />
         </div>
-        <p className = "w-2xl text-lg mx-auto text-gray-300 mt-4">
-          Full-stack developer and designer with a dual focus in Computer Information Systems and Marketing. I build fast, clean web experiences from the ground up.
-        </p>
       </section>
 
       {/* "Explore My Work" Section */}
       <hr className = "w-full border-gray-700 mb-0" />
-
-      <section className = "w-full bg-zinc-800 flex flex-col mb-0">
+      
+      <section className = "w-full bg-radial from-slate-700 to-zinc-950 flex flex-col mb-0">
         <h2 className = "text-3xl font-bold mt-8 mb-8 text-center">Explore My Work</h2>
         
         {/* Buttons Container */}
-        <div className = "w-full flex flex-wrap gap-16 justify-center mx-auto mb-16">
+        <div className = "w-full flex flex-wrap gap-16 justify-center px-6 mx-auto mb-16">
           
           {/* Design Work Button */}
-          <button className = "w-lg h-[800px] rounded-2xl bg-sky-600 hover:scale-105 hover:bg-sky-500 duration-300 flex flex-col">
+          <button className = "w-full max-w-md md:max-w-lg h-[550px] sm:h-[650px] md:h-[800px] rounded-4xl bg-linear-to-b from-fuchsia-600 to-zinc-500 hover:scale-105 hover:bg-sky-500 duration-300 flex flex-col">
             {/* Design Work Button Image */}
-            <div className = "bg-[#28211e] w-sm mx-auto py-8 px-8 rounded-4xl mt-12 rounded-full">
+            <div className = "bg-[#28211e] mx-auto px-4 md:px-8 py-4 md:py-8 rounded-4xl mt-8 lg:mt-12 rounded-full">
               <img
-                src="/Lion Line Art Transparent.png" 
+                src = "/Lion Line Art Transparent.png" 
                 alt = "Designs Preview" 
-                className = "w-80 h-80 object-contain mb-4 mx-auto"
+                className = "w-64 sm:w-80 h-64 sm:h-80 object-contain mb-4 mx-auto"
               />
             </div>
             { /* Design Work Button Text */ }
-            <div className = "flex grow justify-center items-center">
-              <p className = "text-center text-2xl text-white">Design Work</p>
+            <div className = "flex grow flex-col justify-center items-center mx-8 lg:mx-0">
+              <h3 className = "text-center text-3xl md:text-4xl text-white">Design Work</h3>
+              <p className = "max-w-md text-center text-xl md:text-2xl text-gray-300 mt-5 md:mt-10">My design work including Adobe Suite, Canva, and Odd Projects</p>
             </div>  
           </button>
 
           {/* Coding Work Button */}
-          <button className = "w-lg h-[800px] rounded-2xl bg-rose-500 hover:scale-105 hover:bg-rose-600 duration-300 flex flex-col">
-            {/* Coding Work Button Image */}
-            <div className = "bg-[#28211e] w-sm mx-auto py-8 px-8 rounded-4xl mt-12 rounded-full">
+          <button className = "w-full max-w-md md:max-w-lg h-[550px] sm:h-[650px] md:h-[800px] rounded-4xl bg-linear-to-b from-sky-500 to-zinc-500 hover:scale-105 hover:bg-sky-500 duration-300 flex flex-col">
+            {/* Design Work Button Image */}
+            <div className = "bg-[#28211e] mx-auto px-4 md:px-8 py-4 md:py-8 rounded-4xl mt-8 lg:mt-12 rounded-full">
               <img
-                src="/Lion Line Art Transparent.png" 
+                src = "/Lion Line Art Transparent.png" 
                 alt = "Designs Preview" 
-                className = "w-80 h-80 object-contain mb-4 mx-auto"
+                className = "w-64 sm:w-80 h-64 sm:h-80 object-contain mb-4 mx-auto"
               />
             </div>
-            { /* Coding Work Button Text */ }
-            <div className = "flex grow justify-center items-center">
-              <p className = "text-center text-2xl text-white">Coding Work</p>
+            { /* Design Work Button Text */ }
+            <div className = "flex grow flex-col justify-center items-center mx-8 lg:mx-0">
+              <h3 className = "text-center text-3xl md:text-4xl text-white">Coding Work</h3>
+              <p className = "max-w-md text-center text-xl md:text-2xl text-gray-300 mt-5 md:mt-10">My coding work Java GUIs, SQL queries, and Python analytics</p>
             </div>  
           </button>
         </div>
