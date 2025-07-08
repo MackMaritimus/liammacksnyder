dcdbedd49c24065a3558b1de4f01f1d1db8a6dca
Liam Snyder <MackMaritimus@users.noreply.github.com>
2025-07-08 16:37:35 -0600

    Fix for GitHub Image not Loading in Socials

--- src/components/ui/Socials.tsx
+++ src/components/ui/Socials.tsx
@@ -1,41 +1,36 @@
-import Image from "next/image";
+'use client'
+import GitHubIcon from "@/assets/github_invertocat_light.svg";
+import LinkedInIcon from "@/assets/linkedin_white.svg";
+import MailIcon from "@/assets/envelope_white.svg";
+
 
 // Socials Components Containing Linked LinkedIn, GitHub, and Email Icons. Takes className for surrounding Div and icon "width" as params.
 
 interface SocialsProps {
     className?: string;
-    width?: number;
+    width?: string;
 }
 
-export default function Socials ({className = "", width = 9}:SocialsProps) {
+export default function Socials ({className = "", width = "w-9"}:SocialsProps) {
 
     return (
         <div className = {"flex flex-row gap-x-5" + " " + className}>
 
             <a href = "https://www.linkedin.com/in/liammacksnyder" target = "_blank" rel = "noopener noreferrer" className = "flex">
-                <Image
-                    src = "./linkedin_white.svg"
-                    width = {width}
-                    height = {width}
-                    alt = "Liam Mack Snyder LinkedIn"
+                <LinkedInIcon
+                    className = {`${width} Zh-auto`}
                 />
             </a>
 
             <a href = "https://github.com/MackMaritimus" target = "_blank" rel = "noopener noreferrer" className = "flex">
-                <Image
-                    src = "./github_invertocat_light.svg"
-                    width = {width}
-                    height = {width}
-                    alt = "Liam Mack Snyder GitHub"
+                <GitHubIcon
+                    className = {`${width} h-auto`}
                 />
             </a>
 
             <a href = "mailto:LiamMackSnyder@outlook.com" className = "flex">
-                <Image
-                    src = "./envelope_white.svg"
-                    width = {width}
-                    height = {width}
-                    alt = "Liam Mack Snyder Email"
+                <MailIcon
+                    className = {`${width} h-auto`}
                 />
             </a>
             
